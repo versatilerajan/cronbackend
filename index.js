fb0037729365f08823bbb2f5e994dadadeaeb076
Rajan Singh <145154389+versatilerajan@users.noreply.github.com>
2026-07-16 01:02:21 +0530

    Fix formatting of express import statement

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const path = require("path");
-const express = require("express");
+const express=require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const helmet = require("helmet");
